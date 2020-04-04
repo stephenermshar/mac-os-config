1ce9caad5aba29ec290101fcccd23867bbf2212f
Stephen Ermshar <stephen.ermshar@gmail.com>
2020-04-04 16:23:13 -0700

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-# get home directory
-CD="$(pwd)"
-
 # install xcode command line tools
 xcode-select --install
 
-$CD/setup-subscripts/symlinks.sh
-$CD/setup-subscripts/git.sh
-$CD/setup-subscripts/brew.sh
-$CD/setup-subscripts/zsh.sh
-$CD/setup-subscripts/vscode.sh
+SUBSCRIPTS="~/.dotfiles/subscripts"
+
+bash $SUBSCRIPTS/symlinks.sh
+bash $SUBSCRIPTS/brew.sh
+bash $SUBSCRIPTS/zsh.sh
+
